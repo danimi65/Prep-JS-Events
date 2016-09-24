ebc9e9f1def6a599b5b1c846ca44dcef08c690da
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-09-23 19:59:56 -1000

    exercise 3

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
 
 /*Exercise 3. fill in the spaces to make this function operate:*/
   function showAnswer(){
-    document.getElementById(_______).________ = " C (da sea get it?) arrrr!";
+    document.getElementById('three').innerHTML = " C (da sea get it?) arrrr!";
   }
 
 /*Exercise 4.  Instruct the following function to display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
